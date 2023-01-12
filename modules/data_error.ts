d6bb48ab705e954ccc1ee12ba51f3c5c02750784
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-12 21:32:07 +0900

    type infomation

--- modules/data_error.ts
+++ modules/data_error.ts
@@ -1,12 +1,17 @@
 require('dotenv').config();
 
-export async function data_error(): Promise<{ data: string, error: any }> {
+type infomation = {
+    content: string;
+}
+
+export async function data_error(): Promise<{ data: infomation[], error: any }> {
     console.log("data_error invoked")
     try {
         // throw new Error("Error module1");
+        const data = [{ "content": "content 1" }, { "content": "content 2" }]
 
-        return { data: "success", error: null }
+        return { data, error: null }
     } catch (error) {
-        return { data: "", error }
+        return { data: [], error }
     }
 }
