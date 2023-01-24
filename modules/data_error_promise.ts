7852c66f0609e8aaa5d865c387c697eb0cf508e9
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-24 14:18:22 +0900

    f

--- modules/data_error_promise.ts
+++ modules/data_error_promise.ts
@@ -4,11 +4,10 @@ import { beginTransaction, query, commit, end } from '../utils/mysqlPromise';
 import { createConnection } from '../utils/connection';
 import { infomation } from '../types/information';
 
-const connection = createConnection();
-
 export async function data_error_promise(): Promise<{ data: infomation[], error: any }> {
     console.log("data_error_promise invoked")
     try {
+        const connection = createConnection();
         await beginTransaction(connection);
         await query(connection, 'INSERT INTO infomation (content) VALUES (?)', ['Hello!']);
         const data = await query(connection, 'SELECT * FROM infomation', []) as infomation[];
